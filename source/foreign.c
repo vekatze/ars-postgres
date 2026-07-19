2c382d0876d7e12801194b8d056544dfea37edce
vekatze <vekatze@icloud.com>
2026-07-20 06:37:21 +0900

    update dependencies

--- source/foreign.c
+++ source/foreign.c
@@ -3,51 +3,51 @@
 #include <libpq-fe.h>
 
 __attribute__((always_inline))
-int neut_ars_postgres_v0_1_CONNECTION_OK() {
+int neut_ars_postgres_v0_2_CONNECTION_OK() {
   return CONNECTION_OK;
 }
 
 __attribute__((always_inline))
-int neut_ars_postgres_v0_1_PGRES_EMPTY_QUERY() {
+int neut_ars_postgres_v0_2_PGRES_EMPTY_QUERY() {
   return PGRES_EMPTY_QUERY;
 }
 
 __attribute__((always_inline))
-int neut_ars_postgres_v0_1_PGRES_COMMAND_OK() {
+int neut_ars_postgres_v0_2_PGRES_COMMAND_OK() {
   return PGRES_COMMAND_OK;
 }
 
 __attribute__((always_inline))
-int neut_ars_postgres_v0_1_PGRES_TUPLES_OK() {
+int neut_ars_postgres_v0_2_PGRES_TUPLES_OK() {
   return PGRES_TUPLES_OK;
 }
 
 __attribute__((always_inline))
-int neut_ars_postgres_v0_1_PGRES_COPY_OUT() {
+int neut_ars_postgres_v0_2_PGRES_COPY_OUT() {
   return PGRES_COPY_OUT;
 }
 
 __attribute__((always_inline))
-int neut_ars_postgres_v0_1_PGRES_COPY_IN() {
+int neut_ars_postgres_v0_2_PGRES_COPY_IN() {
   return PGRES_COPY_IN;
 }
 
 __attribute__((always_inline))
-int neut_ars_postgres_v0_1_PGRES_BAD_RESPONSE() {
+int neut_ars_postgres_v0_2_PGRES_BAD_RESPONSE() {
   return PGRES_BAD_RESPONSE;
 }
 
 __attribute__((always_inline))
-int neut_ars_postgres_v0_1_PGRES_NONFATAL_ERROR() {
+int neut_ars_postgres_v0_2_PGRES_NONFATAL_ERROR() {
   return PGRES_NONFATAL_ERROR;
 }
 
 __attribute__((always_inline))
-int neut_ars_postgres_v0_1_PGRES_FATAL_ERROR() {
+int neut_ars_postgres_v0_2_PGRES_FATAL_ERROR() {
   return PGRES_FATAL_ERROR;
 }
 
 __attribute__((always_inline))
-int neut_ars_postgres_v0_1_OID_SIZE() {
+int neut_ars_postgres_v0_2_OID_SIZE() {
   return sizeof(Oid);
 }
